1d23929cfa076627df2010f49a1fe39703e9a4f0
Maria <mariaya1995@mail.ru>
2022-12-09 21:27:55 +0300

    lesson4

---
+++ lesson4/my-react/src/API/UserService.js
@@ -0,0 +1,12 @@
+// import axios from "axios";
+// export default class UserService{
+//   static async getAll(){
+//     try{
+//        const respons = await axios.get('https://jsonplaceholder.typicode.com/users')
+//        return respons
+//     }catch(e){
+//         console.log(e)
+//     }
+   
+//   }
+// }
---
+++ lesson4/my-react/src/components/AppRouter.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Route,Routes } from "react-router-dom";
+import Chat from "./pages/Chat";
+import ChatList from "./pages/ChatList";
+import Profile from "./pages/Profile";
+import ChatPage from "./pages/ChatPage";
+
+function AppRouter (){
+    const chatStruct =
+      {
+        "ivan_author": { author: "Иван", id: "ivan_author" },
+        "olga_author": { author: "Ольга", id: "olga_author" }
+      }
+  
+    return (
+          <>
+      <Routes>
+        <Route path ="/profile" element = {<Profile/>}></Route>
+        <Route path ="/chat_bot" element = {<Chat bot_name = {'Вася Бот'}/>}></Route>
+        <Route path ="chat_friend">
+          <Route 
+              path=":chatId" 
+              element={< ChatPage chatStruct={chatStruct} />} 
+          />
+        </Route>
+        <Route path ="/chat_list" element = {<ChatList list = {Object.values(chatStruct)}/>}></Route>
+        <Route path = '*' element = {<h2>404 Page notFOUND</h2>}></Route>
+      </Routes>
+      </> 
+    );
+};
+
+export default AppRouter;
